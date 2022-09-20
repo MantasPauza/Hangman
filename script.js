593522b35fb93c239bc29881dc801cc5330e228e
Mantas <mantaspauza@gmail.com>
2022-09-20 22:10:50 +0300

    fix: :bug: fix word length hint
    fixed h3 element in html to correctly show the hint of word length

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
 const API = "https://random-word-api.herokuapp.com/word";
 const hiddenWordContainer = document.querySelector("#hidden_word");
 const buttons = document.querySelectorAll(".letter_button");
-
-async function hideWord() {
-  const guessingWord = await getGuessingWord();
-  for (let i = 0; i < guessingWord.length; i++) {
-    const div = document.createElement("div");
-    hiddenWordContainer.appendChild(div);
-    div.classList.add('letter');
-    div.setAttribute('id', `letter_${i}`);
-    div.innerText = guessingWord.charAt(i);
-    div.classList.add("hidden");
-  }
-
-  console.log(guessingWord);
-}
-
-
-
-hideWord();
+const titleContainer = document.querySelector(".title");
 
 function getData() {
   return fetch(API).then((response) => response.json());
+}
+
+function run() {
+  // viena funkcija kuri atliks visus veiksmus, zodi padeti i konstanta, ir tada loopinti/appendinti atspeta raide
 }
 
 async function getGuessingWord() {
@@ -29,33 +16,49 @@ async function getGuessingWord() {
   return data.toString();
 }
 
-getGuessingWord();
+async function hideWord() {
+  const guessingWord = await getGuessingWord();
+  for (let i = 0; i < guessingWord.length; i++) {
+    const div = document.createElement("div");
+    hiddenWordContainer.appendChild(div);
+    div.classList.add("letter");
+    div.setAttribute("id", `letter_${i}`);
+    div.innerText = guessingWord.charAt(i);
+    div.classList.add("hidden");
+    div.style.fontWeight = "bold";
+    div.style.fontSize = "35px";
+    div.style.color = "#1A1A1A";
+  }
+
+  const h3 = document.createElement("h3");
+  titleContainer.appendChild(h3);
+  h3.innerText = `Length of the word you are trying to guess is ${guessingWord.length}`;
+
+  console.log(guessingWord);
+}
 
 function checkLetter(e) {
-    const getWord = hiddenWordContainer.innerText;
-    const fixWord =  getWord.replace(/(\r\n|\n|\r)/gm, "");
-    const wordArray = Array.from(fixWord);
-    const letter = e.target.innerText;
-    let indexes = [], i = -1;
-    while ((i = wordArray.indexOf(letter, i+1)) != -1){
-        indexes.push(i);
+  const getWord = hiddenWordContainer.innerText;
+  const fixWord = getWord.replace(/(\r\n|\n|\r)/gm, "");
+  const wordArray = Array.from(fixWord);
+  const letter = e.target.innerText;
+  let indexes = [],
+    i = -1;
+  while ((i = wordArray.indexOf(letter, i + 1)) != -1) {
+    indexes.push(i);
+  }
+
+  indexes.forEach((item) => {
+    if (document.querySelector(`#letter_${item}`)) {
+      document.querySelector(`#letter_${item}`).classList.remove("hidden");
+    } else {
+      return;
     }
+  });
+}
 
-    indexes.forEach((item)=> {
-        document.querySelector(`#letter_${item}`).classList.remove('hidden');
-    })
-
-    /* forEach(letters, (item) => {
-        if (item.innerText === letter) {
-            console.log('yes');
-        }
-        }); */
-    console.log(indexes);
-    console.log(wordArray);
-
-    
-};
-
-buttons.forEach(button => {
-    button.addEventListener("click", checkLetter);
+buttons.forEach((button) => {
+  button.addEventListener("click", checkLetter);
 });
+
+hideWord();
