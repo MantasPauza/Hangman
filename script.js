615d4288ad3bdc30b6b7356b68828612d9e3f0d6
Mantas <mantaspauza@gmail.com>
2022-09-21 19:58:02 +0300

    feat: :sparkles: add functionality to buttons

--- script.js
+++ script.js
@@ -2,63 +2,73 @@ const API = "https://random-word-api.herokuapp.com/word";
 const hiddenWordContainer = document.querySelector("#hidden_word");
 const buttons = document.querySelectorAll(".letter_button");
 const titleContainer = document.querySelector(".title");
-
+const playAgain = document.querySelector(".play_again");
+const h3 = document.createElement("h3");
+const hangManHead = document.querySelector("#hangman_head");
 function getData() {
   return fetch(API).then((response) => response.json());
 }
 
-function run() {
-  // viena funkcija kuri atliks visus veiksmus, zodi padeti i konstanta, ir tada loopinti/appendinti atspeta raide
-}
+playAgain.addEventListener("click", run);
 
-async function getGuessingWord() {
-  const data = await getData();
-  return data.toString();
-}
-
-async function hideWord() {
-  const guessingWord = await getGuessingWord();
-  for (let i = 0; i < guessingWord.length; i++) {
+async function run() {
+  resetBoard(h3, hiddenWordContainer);
+  const word = (await getGuessingWord()).toUpperCase();
+  console.log(word);
+  for (let i = 0; i < word.length; i++) {
     const div = document.createElement("div");
     hiddenWordContainer.appendChild(div);
     div.classList.add("letter");
     div.setAttribute("id", `letter_${i}`);
-    div.innerText = guessingWord.charAt(i);
+    div.innerText = " ";
     div.classList.add("hidden");
     div.style.fontWeight = "bold";
     div.style.fontSize = "35px";
     div.style.color = "#1A1A1A";
   }
 
-  const h3 = document.createElement("h3");
   titleContainer.appendChild(h3);
-  h3.innerText = `Length of the word you are trying to guess is ${guessingWord.length}`;
+  h3.innerText = `Length of the word you are trying to guess is ${word.length}`;
+  /* console.log(word); */
 
-  console.log(guessingWord);
-}
+  const wordArray = Array.from(word);
+  const triesLeft = 5;
 
-function checkLetter(e) {
-  const getWord = hiddenWordContainer.innerText;
-  const fixWord = getWord.replace(/(\r\n|\n|\r)/gm, "");
-  const wordArray = Array.from(fixWord);
-  const letter = e.target.innerText;
-  let indexes = [],
-    i = -1;
-  while ((i = wordArray.indexOf(letter, i + 1)) != -1) {
-    indexes.push(i);
-  }
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const letter = button.innerText;
+      let indexes = [], 
+      i = -1;
+      while ((i = wordArray.indexOf(letter, i + 1)) != -1) {
+        indexes.push(i);
+        console.log(indexes);
+        console.log(word);
+        console.log(wordArray);
+      }
+      button.setAttribute("disabled", true);
 
-  indexes.forEach((item) => {
-    if (document.querySelector(`#letter_${item}`)) {
-      document.querySelector(`#letter_${item}`).classList.remove("hidden");
-    } else {
-      return;
-    }
+      indexes.forEach((item) => {
+        if (document.querySelector(`#letter_${item}`)) {
+          document.querySelector(`#letter_${item}`).classList.remove("hidden");
+          document.querySelector(`#letter_${item}`).innerText =
+            word[item].toUpperCase();
+          document.querySelector(`#letter_${item}`).classList.add("correct");
+        } else {
+          return;
+        }
+      });
+    });
   });
 }
+async function getGuessingWord() {
+  const data = await getData();
+  return data.toString();
+}
 
-buttons.forEach((button) => {
-  button.addEventListener("click", checkLetter);
-});
-
-hideWord();
+function resetBoard(elem1, elem2) {
+  elem1.innerText = "";
+  elem2.innerText = "";
+  buttons.forEach((button) => {
+    button.removeAttribute('disabled');
+  })
+}
